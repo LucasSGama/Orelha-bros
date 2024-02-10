fe8ee470687bbdec33b5ee6269feff9ffe0be92e
Lucas-Da-Gama <lucasantosgama3@gmail.com>
2024-02-10 18:37:17 -0300

    Volta do game

--- JS/script.js
+++ JS/script.js
@@ -1,57 +1,16 @@
-    const mario = document.querySelector('.mario');
-    const pipe = document.querySelector('.pipe');
-    const score = document.querySelector('.score');
-    const highscore = document.querySelector('.highscore');
+const mario = document.getElementById('mario');
+const pipe = document.getElementById('pipe');
 
+document.addEventListener('keydown', function(event) {
+    var bottomValue = parseInt(getComputedStyle(mario).bottom);
 
-    document.getElementById('botao').addEventListener('click', function() {
-        window.location.href = window.location.origin + window.location.pathname
-    });
+    if (event.key === ' ' && bottomValue === 0) {
+        mario.classList.add('jump');
 
-    // function RecarregarPagina() {
-    //     location.reload();
-    // }
-
-const jump = () => {
-    mario.classList.add('jump');
-
-    setTimeout(() => {
-        mario.classList.remove('jump');
-    }, 500)
-}
-
-
-
-
-
-// REGISTRO DE MORTE __________________________________________________________
-
-
-setInterval(() => {
-
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-
-    console.log(marioPosition)
-    
-    
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
-        
-        mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
-
-        mario.src = 'images/game-over-richard.png';
-        mario.style.width = '75px';
-        mario.style.marginleft = '50px'
-
-        
-        alert('Game over! Seu score foi' ${score});
-        score = 0;
-
+        // Adiciona um temporizador para remover a classe após 500 milissegundos (ajuste conforme necessário)
+        setTimeout(function() {
+            mario.classList.remove('jump');
+        }, 500);
     }
-}, 10)
+});
 
-document.addEventListener('keydown', jump);
