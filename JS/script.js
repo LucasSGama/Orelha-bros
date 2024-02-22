2589f1d44830abc83406534b35e59636ba7ed1fa
Lucas-Da-Gama <lucasantosgama3@gmail.com>
2024-02-22 09:47:00 -0300

    Atualização

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,29 @@
 const mario = document.getElementById('mario');
 const pipe = document.getElementById('pipe');
+const clouds = document.querySelector('.clouds')
 
 document.addEventListener('keydown', function(event) {
     var bottomValue = parseInt(getComputedStyle(mario).bottom);
 
     if (event.key === ' ' && bottomValue === 0) {
         mario.classList.add('jump');
-
+ 
         // Adiciona um temporizador para remover a classe após 500 milissegundos (ajuste conforme necessário)
         setTimeout(function() {
             mario.classList.remove('jump');
         }, 500);
     }
+
+    if (event.key === ' ') {
+        pipe.classList.add('pipe-move');
+        clouds.classList.add('clouds-animation');
+        console.log("Ta puro");
+
+        const h3 = document.getElementById('Aviso');
+        h3.style.display = "none";
+ 
+    }
+     
 });
 
+ 
